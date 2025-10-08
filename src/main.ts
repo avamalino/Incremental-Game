26b04b1c48e449148b2ea263c7db9ce42654e238
Ava :3 <138967687+avamalino@users.noreply.github.com>
2025-10-08 17:09:43 +0000

    section 2 assignment Co-authored-by: nabhatta <nabhatta@users.noreply.github.com> Co-authored-by: Neila Miranda <BellaTheAlien@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,16 @@ import "./style.css";
 document.body.innerHTML = `
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
 `;
+console.log("herro");
+console.log(":3");
+const button = document.createElement('button');
+button.textContent = "💜"; //place your emoji
+button.id = "clicker"; //for the next step, i think
+<div id="clicker"></div>
+document.body.appendChild(button);
+
+let counter: number = 0;
+
+button.addEventListener('click', () =&gt; {
+  console.log('Button clicked');
+});
